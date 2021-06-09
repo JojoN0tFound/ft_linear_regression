23c5de8a399b5f8f3141c955f8a6cca80976256a
jojoquivogn <jquivogn@student.42.fr>
2021-06-09 18:03:08 +0200

    fix thetas bug

--- predict.py
+++ predict.py
@@ -150,7 +150,9 @@ def just_compare(args):
 		data[0] = csv[0]
 		data[1] = csv[1]
 	for i in range(len(csv[0])):
-		if weights['ngd'] == False:
+		if weights['t0'] == 0.0 and weights['ngd'] == 0.0:
+			predict = 0.0
+		elif weights['ngd'] == False:
 			predict = unnormalize(predict_km(data[0][i], weights), data[1])
 		else:
 			predict = predict_km(data[0][i], weights)
@@ -175,7 +177,9 @@ def predict():
 	km = get_km()
 	ask_for_train()
 	weights = get_weights()
-	if weights['ngd'] == False or (weights['t0'] != 0.0 and weights['ngd'] != 0.0):
+	if weights['t0'] == 0.0 and weights['ngd'] == 0.0:
+		predict = 0.0
+	elif weights['ngd'] == False:
 		csv = get_csv('data.csv')
 		data = normalize_data(km, csv)
 		predict = predict_km(data[0][0], weights)
